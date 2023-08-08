ff11d4df6ffc84efe441024d0c3cc32c885041dc
Gunjan Khanal <thisisgunjan@yahoo.com>
2023-08-08 17:02:41 +0545

    Update manifestupdater.sh

--- manifestupdater.sh
+++ manifestupdater.sh
@@ -2,8 +2,8 @@
 git config user.email "thisisgunjan@yahoo.com"
 git config user.name "gunjankhanal"
 
-sed -i "s/goonz\/azure_push:/goonz\/azure_push:${BUILD_ID}/g" deploymentmanifest/deployment.yaml
+sed -i "s/goonz\/azure_push:*/goonz\/azure_push:${BUILD_ID}/" deploymentmanifest/deployment.yaml
 git add deploymentmanifest/deployment.yaml
-git commit -m "Update deployment image to version $(BUILD_ID)"
+git commit -m "Update deployment image to version ${BUILD_ID}"
 
 git push https://${GITHUB_TOKEN}@github.com/${GIT_USER_NAME}/${GIT_REPO_NAME} HEAD:master
