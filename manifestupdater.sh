cf54f4ce66c735b4cfc32e464474b4631292f3cd
Gunjan Khanal <thisisgunjan@yahoo.com>
2023-08-08 19:17:19 +0545

    Update manifestupdater.sh

--- manifestupdater.sh
+++ manifestupdater.sh
@@ -7,3 +7,14 @@ git add deploymentmanifest/deployment.yaml
 git commit -m "Update deployment image to version ${BUILD_ID}"
 
 git push https://${GITHUB_TOKEN}@github.com/${GIT_USER_NAME}/${GIT_REPO_NAME} HEAD:master
+
+
+current_version=${BUILD_ID}
+# Increment the version
+new_version=$((current_version + 1))
+
+# Write the new version back to the file
+echo $new_version > BUILD_ID
+
+# Display the new version
+echo "New version: $new_version"
