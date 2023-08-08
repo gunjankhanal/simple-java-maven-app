4d3ed28cfa8fb6a05bf810d70788e60796d7a4c0
Gunjan Khanal <thisisgunjan@yahoo.com>
2023-08-08 15:23:09 +0545

    Update manifestupdater.sh

--- manifestupdater.sh
+++ manifestupdater.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git config user.email "thisisgunjan@yahoo.com"
 git config user.name "gunjankhanal"
-BuildId=${BuildId}
-sed -i "s/replaceImageTag/${BUILD_NUMBER}/g" deploymentmanifest/deployment.yaml
+{Build.BuildId}=${Build.BuildId}
+sed -i "s/replaceImageTag/${Build.BuildNumber}/g" deploymentmanifest/deployment.yaml
 git add deploymentmanifest/deployment.yaml
 git commit -m "Update deployment image to version ${BUILD_NUMBER}"
 git push https://${GITHUB_TOKEN}@github.com/${GIT_USER_NAME}/${GIT_REPO_NAME} HEAD:master
